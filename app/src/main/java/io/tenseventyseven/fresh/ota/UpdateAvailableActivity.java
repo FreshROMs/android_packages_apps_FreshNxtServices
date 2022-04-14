d20555afa2240f197316bef2ebd9ecd25d939e56
John Vincent <git@tenseventyseven.cf>
2022-04-14 19:02:57 +0800

    zest: Initial work for UpdateAvailableActivity
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

---
+++ app/src/main/java/io/tenseventyseven/fresh/ota/UpdateAvailableActivity.java
@@ -0,0 +1,17 @@
+package io.tenseventyseven.fresh.ota;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import io.tenseventyseven.fresh.R;
+
+public class UpdateAvailableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_screen);
+    }
+
+}
