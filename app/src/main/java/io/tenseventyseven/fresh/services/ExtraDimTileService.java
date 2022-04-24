e38171069a0111e353d760b623bb6b43fa6b5033
John Vincent <git@tenseventyseven.cf>
2022-04-24 15:31:13 +0800

    zest: ExtraDim: Add QS detail API

--- app/src/main/java/io/tenseventyseven/fresh/services/ExtraDimTileService.java
+++ app/src/main/java/io/tenseventyseven/fresh/services/ExtraDimTileService.java
@@ -1,8 +1,12 @@
 package io.tenseventyseven.fresh.services;
 
+import android.content.Intent;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
+import android.widget.RemoteViews;
 
+import io.tenseventyseven.fresh.PerformanceUtils;
+import io.tenseventyseven.fresh.R;
 import io.tenseventyseven.fresh.zest.sub.ExtraDimSettingsActivity;
 
 public class ExtraDimTileService extends TileService {
@@ -21,6 +25,33 @@ public class ExtraDimTileService extends TileService {
     public void onClick() {
         ExtraDimSettingsActivity.setExtraDimState(this, !mExtraDimState);
         setTileState(!mExtraDimState);
+    }
+
+    public RemoteViews semGetDetailView() {
+        RemoteViews view = new RemoteViews(getPackageName(), R.layout.zest_qs_detail_extra_dim);
+        return view;
+    }
+
+    public CharSequence semGetDetailViewTitle() {
+        return getString(R.string.zest_extra_dim_setting_title);
+    }
+
+    public Intent semGetSettingsIntent() {
+        Intent intent = new Intent(this, ExtraDimSettingsActivity.class);
+        return intent;
+    }
+
+    public boolean semIsToggleButtonChecked() {
+        return ExtraDimSettingsActivity.getExtraDimState(this);
+    }
+
+    public void semSetToggleButtonChecked(boolean z) {
+        ExtraDimSettingsActivity.setExtraDimState(this, z);
+        setTileState(z);
+    }
+
+    public boolean semIsToggleButtonExists() {
+        return true;
     }
 
     private void setTileState(boolean state) {
