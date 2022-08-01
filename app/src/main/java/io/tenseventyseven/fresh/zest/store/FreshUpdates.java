a963c4beb1890ad537ae19560c978ddd67448128
Yanndroid <yanndroid.dev@gmail.com>
2022-08-01 02:40:15 +0200

    zest: fresh updates (part 1)

---
+++ app/src/main/java/io/tenseventyseven/fresh/zest/store/FreshUpdates.java
@@ -0,0 +1,298 @@
+package io.tenseventyseven.fresh.zest.store;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.IPackageDeleteObserver;
+import android.content.pm.PackageInstaller;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.tonyodev.fetch2.AbstractFetchListener;
+import com.tonyodev.fetch2.Download;
+import com.tonyodev.fetch2.Error;
+import com.tonyodev.fetch2.Fetch;
+import com.tonyodev.fetch2.FetchConfiguration;
+import com.tonyodev.fetch2.NetworkType;
+import com.tonyodev.fetch2.Priority;
+import com.tonyodev.fetch2.Request;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+
+public class FreshUpdates {
+
+    private static final String FETCH_INSTANCE_NAME = "FreshUpdates";
+    private static final String LIST_URL = "https://yanndroid.github.io/fresh-updates-test/updates.json"; //"https://ota.fresh.tenseventyseven.cf/a50/fresh_updates/";
+
+    public static class Update {
+        public String name, packageName, summary, changelog, versionName, fileUrl, iconUrl;
+        public long versionCode, fileSize;
+    }
+
+    public interface ResultListener {
+        void onSuccess(Object o);
+
+        void onFailed();
+    }
+
+    public interface DownloadListener extends ResultListener {
+        void onProgress(int progress, long eta, long speed);
+    }
+
+    public static void getStoreList(ResultListener listener) {
+        Handler handler = new Handler(Looper.getMainLooper());
+        Executors.newSingleThreadExecutor().execute(() -> {
+            try {
+                URLConnection connection = new URL(LIST_URL).openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) throw new Exception("null");
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) buffer.append(line)/*.append("\n")*/;
+                if (buffer.length() == 0) throw new Exception("null");
+
+                handler.post(() -> {
+                    try {
+                        listener.onSuccess(parseJson(buffer.toString()));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        listener.onFailed();
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+                handler.post(listener::onFailed);
+            }
+        });
+    }
+
+    private static ArrayList<Update> parseJson(String response) throws JSONException {
+        ArrayList<Update> list = new ArrayList<>();
+        JSONArray jsonList = new JSONObject(response).getJSONArray("response");
+        for (int i = 0; i < jsonList.length(); i++) {
+            Update listItem = new Update();
+            JSONObject jsonItem = jsonList.getJSONObject(i);
+            listItem.name = jsonItem.optString("name");
+            listItem.packageName = jsonItem.optString("packageName");
+            listItem.summary = jsonItem.optString("summary");
+            listItem.changelog = jsonItem.optString("changelog");
+            listItem.name = jsonItem.optString("name");
+            listItem.versionName = jsonItem.optString("versionName");
+            listItem.fileUrl = jsonItem.optString("file");
+            listItem.iconUrl = jsonItem.optString("icon");
+            listItem.versionCode = jsonItem.optInt("versionCode");
+            listItem.fileSize = jsonItem.optLong("size");
+            list.add(listItem);
+        }
+        return list;
+    }
+
+    public static boolean isDeviceOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm == null) return false;
+
+        NetworkCapabilities capabilities = cm.getNetworkCapabilities(cm.getActiveNetwork());
+        if (capabilities == null) return false;
+
+        return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN) ||
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET);
+    }
+
+    public static boolean isConnectionUnmetered(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm == null) return false;
+
+        NetworkCapabilities capabilities = cm.getNetworkCapabilities(cm.getActiveNetwork());
+        if (capabilities == null) return false;
+
+        return isDeviceOnline(context) && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED);
+    }
+
+    public static void downloadFile(Context context, Update update, DownloadListener listener) {
+        File file = new File(context.getFilesDir(), update.packageName + "_" + update.versionName + ".apk");
+        if (file.exists()) file.delete();
+
+        final Request request = new Request(update.fileUrl, file.getPath());
+        request.setPriority(Priority.HIGH);
+        request.setNetworkType(NetworkType.ALL);
+
+        Fetch fetch = getFetch(context);
+        fetch.enqueue(request, null, null);
+        fetch.addListener(new AbstractFetchListener() {
+            @Override
+            public void onCompleted(@NonNull Download download) {
+                listener.onSuccess(file);
+            }
+
+            @Override
+            public void onError(@NonNull Download download, @NonNull Error error, @Nullable Throwable throwable) {
+                listener.onFailed();
+            }
+
+            @Override
+            public void onProgress(@NonNull Download download, long etaInMilliSeconds, long downloadedBytesPerSecond) {
+                listener.onProgress(download.getProgress(), etaInMilliSeconds, downloadedBytesPerSecond);
+            }
+        });
+    }
+
+    private static Fetch getFetch(Context context) {
+        FetchConfiguration.Builder fc = new FetchConfiguration.Builder(context)
+                .setDownloadConcurrentLimit(1)
+                .setAutoRetryMaxAttempts(5)
+                .setNamespace(FETCH_INSTANCE_NAME)
+                .enableLogging(true)
+                .enableAutoStart(true);
+
+        return Fetch.Impl.getInstance(fc.build());
+    }
+
+    public static String getFormattedFileSize(long fileSize) {
+        if (fileSize <= 0) return "0B";
+        final String[] units = new String[]{"B", "KB", "MB", "GB", "TB", "PB", "EB"};
+        int digitGroups = (int) (Math.log10(fileSize) / Math.log10(1000));
+        return new DecimalFormat("#,##0.#").format(fileSize / Math.pow(1000, digitGroups)) + " " + units[digitGroups];
+    }
+
+    public static String getFormattedSpeed(long fileSize) {
+        return String.format("%s/s", getFormattedFileSize(fileSize));
+    }
+
+    public static boolean isPackageInstalled(Context context, String packageName) {
+        return context.getPackageManager().isPackageAvailable(packageName);
+    }
+
+    public static long getPackageVersionCode(Context context, String packageName) {
+        try {
+            return context.getPackageManager().getPackageInfo(packageName, 0).getLongVersionCode();
+        } catch (PackageManager.NameNotFoundException e) {
+            return 0;
+        }
+    }
+
+    public static String getPackageVersionName(Context context, String packageName) {
+        try {
+            return context.getPackageManager().getPackageInfo(packageName, 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            return "-";
+        }
+    }
+
+    public static void installPackage(Context context, File packageFile, ResultListener listener) {
+        try {
+            PackageInstaller packageInstaller = context.getPackageManager().getPackageInstaller();
+            packageInstaller.registerSessionCallback(new PackageInstaller.SessionCallback() {
+                @Override
+                public void onCreated(int sessionId) {
+                }
+
+                @Override
+                public void onBadgingChanged(int sessionId) {
+                }
+
+                @Override
+                public void onActiveChanged(int sessionId, boolean active) {
+                }
+
+                @Override
+                public void onProgressChanged(int sessionId, float progress) {
+                }
+
+                @Override
+                public void onFinished(int sessionId, boolean success) {
+                    if (success) listener.onSuccess(null);
+                    else listener.onFailed();
+                }
+            });
+            PackageInstaller.Session session = packageInstaller.openSession(packageInstaller.createSession(new PackageInstaller.SessionParams(PackageInstaller.SessionParams.MODE_FULL_INSTALL)));
+
+            FileInputStream in = new FileInputStream(packageFile);
+            OutputStream out = session.openWrite("FreshUpdates", 0, -1);
+            byte[] buffer = new byte[1048576];
+            int read;
+            while ((read = in.read(buffer)) > 0) out.write(buffer, 0, read);
+            in.close();
+            out.flush();
+            out.close();
+
+            session.commit(PendingIntent.getActivity(context, 0, new Intent(), 0).getIntentSender());
+        } catch (IOException e) {
+            e.printStackTrace();
+            listener.onFailed();
+        }
+
+    }
+
+    public static void deletePackage(Context context, String packageName, ResultListener listener) {
+        //todo: Option 1 - this works without package installer | requires android.permission.DELETE_PACKAGES
+        /*try {
+            PackageManager pm = context.getPackageManager();
+            pm.getClass().getMethod("deletePackage", String.class, IPackageDeleteObserver.class, int.class)
+                    .invoke(pm, packageName, new IPackageDeleteObserver() {
+                        @Override
+                        public void packageDeleted(String packageName, int returnCode) {
+                            if (returnCode == PackageManager.DELETE_SUCCEEDED) {
+                                listener.onSuccess(null);
+                            } else {
+                                listener.onFailed();
+                            }
+                        }
+
+                        @Override
+                        public IBinder asBinder() {
+                            return null;
+                        }
+                    }, 0);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }*/
+
+        //todo: Option 2 - using package installer but no callback -> list item won't update
+        Intent intent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE);
+        intent.setData(Uri.parse("package:" + packageName));
+        intent.putExtra(Intent.EXTRA_RETURN_RESULT, true);
+        context.startActivity(intent);
+    }
+
+    public static boolean canLaunchApp(Context context, String packageName) {
+        return context.getPackageManager().getLaunchIntentForPackage(packageName) != null;
+    }
+
+    public static void launchApp(Context context, String packageName) {
+        Intent launch = context.getPackageManager().getLaunchIntentForPackage(packageName);
+        if (launch == null) return;
+        launch.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(launch);
+    }
+
+}
---
+++ app/src/main/java/io/tenseventyseven/fresh/zest/store/FreshUpdatesActivity.java
@@ -0,0 +1,274 @@
+package io.tenseventyseven.fresh.zest.store;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.icu.text.SimpleDateFormat;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.dlyt.yanndroid.oneui.dialog.ProgressDialog;
+import de.dlyt.yanndroid.oneui.layout.ToolbarLayout;
+import de.dlyt.yanndroid.oneui.sesl.recyclerview.LinearLayoutManager;
+import de.dlyt.yanndroid.oneui.view.RecyclerView;
+import de.dlyt.yanndroid.oneui.widget.ProgressBar;
+import io.tenseventyseven.fresh.R;
+
+public class FreshUpdatesActivity extends AppCompatActivity {
+
+    @BindView(R.id.zest_fresh_updates_toolbar)
+    ToolbarLayout toolbar;
+
+    @BindView(R.id.zest_fresh_updates_list)
+    RecyclerView listView;
+
+    private ProgressDialog progressDialog;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.zest_activity_fresh_updates);
+        ButterKnife.bind(this);
+
+        toolbar.setNavigationButtonTooltip(getString(R.string.sesl_navigate_up));
+        toolbar.setNavigationButtonOnClickListener(v -> onBackPressed());
+        setSupportActionBar(toolbar.getToolbar());
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_CIRCLE);
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
+        FreshUpdates.getStoreList(new FreshUpdates.ResultListener() {
+            @Override
+            public void onSuccess(Object o) {
+                progressDialog.dismiss();
+                ArrayList<FreshUpdates.Update> list = (ArrayList<FreshUpdates.Update>) o;
+                initListView(list);
+                toolbar.setSubtitle(String.valueOf(list.size()));
+            }
+
+            @Override
+            public void onFailed() {
+                progressDialog.dismiss();
+            }
+        });
+
+    }
+
+    private void initListView(ArrayList<FreshUpdates.Update> list) {
+        listView.setLayoutManager(new LinearLayoutManager(this));
+        listView.setAdapter(new UpdatesAdapter(this, list));
+        listView.addItemDecoration(new ItemDecoration(this));
+        listView.setItemAnimator(null);
+        listView.seslSetFillBottomEnabled(true);
+        listView.seslSetLastRoundedCorner(true);
+        listView.seslSetFastScrollerEnabled(true);
+        listView.seslSetGoToTopEnabled(true);
+        listView.seslSetSmoothScrollEnabled(true);
+    }
+
+    public class UpdatesAdapter extends RecyclerView.Adapter<UpdatesAdapter.ViewHolder> {
+        private Context mContext;
+        private ArrayList<FreshUpdates.Update> mList;
+
+        UpdatesAdapter(Context context, ArrayList<FreshUpdates.Update> list) {
+            this.mContext = context;
+            this.mList = list;
+        }
+
+        @Override
+        public int getItemCount() {
+            return mList.size();
+        }
+
+        @NonNull
+        @Override
+        public UpdatesAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new UpdatesAdapter.ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.zest_fresh_updates_list_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(UpdatesAdapter.ViewHolder holder, final int position) {
+            final FreshUpdates.Update update = mList.get(position);
+
+            holder.uIcon.setClipToOutline(true);
+            Picasso.get().load(update.iconUrl).noPlaceholder().into(holder.uIcon, new Callback() {
+                @Override
+                public void onSuccess() {
+                    holder.uLoad.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    holder.uLoad.setVisibility(View.GONE);
+                    holder.uIcon.setImageResource(R.drawable.zest_fresh_updates_no_icon);
+                }
+            });
+
+            holder.uName.setText(update.name);
+            holder.uPackage.setText(update.packageName);
+            holder.uVersion.setText(getText(R.string.fresh_ota_changelog_detail_version) + " " + FreshUpdates.getPackageVersionName(mContext, update.packageName));
+            holder.uSummary.setText(update.summary);
+
+            if (update.versionCode > FreshUpdates.getPackageVersionCode(mContext, update.packageName)) {
+                holder.aDownload.setVisibility(View.VISIBLE);
+                holder.aDownload.setOnClickListener(v -> {
+                    holder.aContainer.setVisibility(View.GONE);
+                    holder.pContainer.setVisibility(View.VISIBLE);
+                    holder.pProgress.setIndeterminate(false);
+                    holder.pProgress.setProgress(0);
+                    holder.pETA.setText(null);
+                    holder.pSpeed.setText(null);
+
+                    FreshUpdates.downloadFile(mContext, update, new FreshUpdates.DownloadListener() {
+                        @Override
+                        public void onProgress(int progress, long eta, long speed) {
+                            holder.pProgress.setProgress(progress);
+                            holder.pETA.setText(new SimpleDateFormat("hh:mm:ss").format(new Date(eta)));
+                            holder.pSpeed.setText(FreshUpdates.getFormattedSpeed(speed));
+                        }
+
+                        @Override
+                        public void onFailed() {
+                            holder.pContainer.setVisibility(View.GONE);
+                            holder.aContainer.setVisibility(View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onSuccess(Object o) {
+                            holder.pETA.setText(null);
+                            holder.pSpeed.setText(null);
+                            holder.pProgress.setIndeterminate(true);
+
+                            FreshUpdates.installPackage(mContext, (File) o, new FreshUpdates.ResultListener() {
+                                @Override
+                                public void onSuccess(Object o) {
+                                    notifyItemChanged(position);
+                                    holder.pContainer.setVisibility(View.GONE);
+                                    holder.aContainer.setVisibility(View.VISIBLE);
+                                }
+
+                                @Override
+                                public void onFailed() {
+                                    holder.pContainer.setVisibility(View.GONE);
+                                    holder.aContainer.setVisibility(View.VISIBLE);
+                                }
+                            });
+                        }
+                    });
+                });
+
+                holder.cContainer.setVisibility(View.VISIBLE);
+                holder.cTitle.setText(update.versionName + " - " + getText(R.string.fresh_ota_changelog_header_title));
+                holder.cSummary.setText(update.changelog);
+                holder.cSize.setText(getText(R.string.fresh_ota_changelog_detail_size) + " " + FreshUpdates.getFormattedFileSize(update.fileSize));
+            } else {
+                holder.aDownload.setVisibility(View.GONE);
+                holder.aDownload.setOnClickListener(null);
+                holder.cContainer.setVisibility(View.GONE);
+            }
+
+            holder.aLaunch.setVisibility(FreshUpdates.canLaunchApp(mContext, update.packageName) ? View.VISIBLE : View.GONE);
+            holder.aLaunch.setOnClickListener(v -> FreshUpdates.launchApp(mContext, update.packageName));
+
+            holder.aDelete.setVisibility(FreshUpdates.isPackageInstalled(mContext, update.packageName) ? View.VISIBLE : View.GONE);
+            holder.aDelete.setOnClickListener(v -> {
+                holder.aContainer.setVisibility(View.GONE);
+                FreshUpdates.deletePackage(mContext, update.packageName, new FreshUpdates.ResultListener() {
+                    @Override
+                    public void onSuccess(Object o) {
+                        holder.aContainer.setVisibility(View.VISIBLE);
+                        notifyItemChanged(position);
+                    }
+
+                    @Override
+                    public void onFailed() {
+                        holder.aContainer.setVisibility(View.VISIBLE);
+                    }
+                });
+            });
+
+            //todo get fetch downloader + progress
+            holder.aContainer.setVisibility(View.VISIBLE);
+
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            ImageView uIcon;
+            ProgressBar uLoad;
+            TextView uName, uPackage, uVersion, uSummary;
+            LinearLayout aContainer;
+            ImageView aLaunch, aDownload, aDelete;
+            LinearLayout cContainer;
+            TextView cTitle, cSummary, cSize;
+            LinearLayout pContainer;
+            ProgressBar pProgress;
+            TextView pSpeed, pETA;
+
+            ViewHolder(View itemView) {
+                super(itemView);
+                uIcon = itemView.findViewById(R.id.fresh_update_icon);
+                uLoad = itemView.findViewById(R.id.fresh_update_loading);
+                uName = itemView.findViewById(R.id.fresh_update_title);
+                uPackage = itemView.findViewById(R.id.fresh_update_package);
+                uVersion = itemView.findViewById(R.id.fresh_update_version);
+                uSummary = itemView.findViewById(R.id.fresh_update_summary);
+                aContainer = itemView.findViewById(R.id.fresh_update_actions);
+                aLaunch = itemView.findViewById(R.id.fresh_update_launch);
+                aDownload = itemView.findViewById(R.id.fresh_update_download);
+                aDelete = itemView.findViewById(R.id.fresh_update_delete);
+                cContainer = itemView.findViewById(R.id.fresh_update_changelog);
+                cTitle = itemView.findViewById(R.id.fresh_update_changelog_title);
+                cSummary = itemView.findViewById(R.id.fresh_update_changelog_summary);
+                cSize = itemView.findViewById(R.id.fresh_update_changelog_size);
+                pContainer = itemView.findViewById(R.id.fresh_update_progress);
+                pProgress = itemView.findViewById(R.id.fresh_update_progress_bar);
+                pSpeed = itemView.findViewById(R.id.fresh_update_progress_speed);
+                pETA = itemView.findViewById(R.id.fresh_update_progress_eta);
+            }
+        }
+    }
+
+    private class ItemDecoration extends RecyclerView.ItemDecoration {
+        private final Drawable mDivider;
+
+        public ItemDecoration(@NonNull Context context) {
+            mDivider = context.getDrawable(R.drawable.sesl_list_divider);
+        }
+
+        @Override
+        public void onDraw(@NonNull Canvas c, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+            super.onDraw(c, parent, state);
+
+            for (int i = 0; i < parent.getChildCount(); i++) {
+                View child = parent.getChildAt(i);
+                final int top = child.getBottom()
+                        + ((ViewGroup.MarginLayoutParams) child.getLayoutParams()).bottomMargin;
+                final int bottom = mDivider.getIntrinsicHeight() + top;
+
+                mDivider.setBounds(parent.getLeft(), top, parent.getRight(), bottom);
+                mDivider.draw(c);
+            }
+        }
+    }
+
+}
